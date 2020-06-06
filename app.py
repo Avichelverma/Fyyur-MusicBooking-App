676c4ccade6a839cc8e48d75750abb22e15dfce9
Avichel Verma <averma18@hawk.iit.edu>
2020-06-06 17:00:15 -0500

    venues/create implemented

--- app.py
+++ app.py
@@ -342,9 +342,29 @@ def create_venue_form():
 def create_venue_submission():
     # TODO: insert form data as a new Venue record in the db, instead
     # TODO: modify data to be the data object returned from db insertion
+    try:
+    # get form data and create 
+        form = VenueForm()
+        venue = Venue(name=form.name.data, city=form.city.data, state=form.state.data, address=form.address.data,
+                    phone=form.phone.data, image_link=form.image_link.data,genres=form.genres.data, 
+                    facebook_link=form.facebook_link.data, seeking_description=form.seeking_description.data,
+                    website=form.website.data, seeking_talent=form.seeking_talent.data)
+        
+        # commit session to database
+        db.session.add(venue)
+        db.session.commit()
 
-    # on successful db insert, flash success
-    flash('Venue ' + request.form['name'] + ' was successfully listed!')
+        # flash success 
+        flash('Venue ' + request.form['name'] + ' was successfully listed!')
+    except:
+        # catches errors
+        db.session.rollback()
+        flash('An error occurred. Venue ' + request.form['name'] + ' could not be listed.')
+    finally:
+        # closes session
+        db.session.close()
+
+
     # TODO: on unsuccessful db insert, flash an error instead.
     # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
     # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
